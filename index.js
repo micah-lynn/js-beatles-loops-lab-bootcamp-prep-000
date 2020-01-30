3dd8e2fade42f12fa1d042ad679fbd87d6eac3a6
Micah Lynn <micah@sabado.com>
2020-01-30 16:08:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function johnLennonFacts(array) {
   var emptyarray = [];
   var count = array.length
   while (count > 0) {
-    emptyarray.push(`array[i] + "!!!"`);
+    emptyarray.push(`${array[i]} + "!!!"`);
     i++;
   }
   return array;
