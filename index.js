19bc6bd4f58497271b53625e98f3aead2172cf1c
Micah Lynn <micah@sabado.com>
2020-01-30 15:58:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var listBeatles = [];
+  for (var i = 0; i < musicians.length; i++) {
+    listBeatles.push(musicians[i] + ' plays ' + instruments[i]);
+  }
+  return listBeatles;
+}
+
+function johnLennonFacts() {
+  var facts = ["He was the last Beatle to learn to drive","He was never a vegetarian","He was a choir boy and boy scout","He hated the sound of his own voice"];
+  var array = [];
+  var i = 0;
+  while (array.length < facts.length) {
+    array.push(facts[i] + "!!!");
+    i++;
+  }
+  return array;
+}
